0f4f836b9aea9ba752a0e1819b922811317ee53a
vadranamakhila <42572588+vadranamakhila@users.noreply.github.com>
2018-09-07 11:14:00 +0530

    Update power.py

--- power.py
+++ power.py
@@ -1,4 +1,4 @@
-import math
-num=int(input("enter the number:"))
-pwr=int(input("enter the power:"))
-print (round(math.pow(num, pwr)))
+x=int(input("enter the number:"))
+y=int(input("enter the power:"))
+z=x**y;
+print "sum of x**y =", z;
